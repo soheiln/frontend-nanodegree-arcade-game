55b5106e138728b563cb4ebca285d34afc95a6b6
Soheil Norouzi <soheil.nrz@gmail.com>
2015-07-31 13:38:25 -0700

    Implement Player class and game login in app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
+//Game Configuration
+var gameConfig = {
+    rowSpace: 83,
+    colSpace: 101,
+    rowOffset: 50,
+    colOffset: 50,
+    numEnemies: 6,
+    numEnemyRows: 3,
+    enemyStartX: -400,
+    enemyEndX: 600,
+    enemySpeedRange: 500,
+    collisionThreshold: {
+        x: 40,
+        y: 10
+    }
+}
+
+
 // Enemies our player must avoid
 var Enemy = function() {
     // Variables applied to each of our instances go here,
@@ -6,6 +24,9 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.speed = this._getRandomSpeed();
+    this.x = gameConfig.enemyStartX;
+    this.y = this._getRandomY();
 }
 
 // Update the enemy's position, required method for game
@@ -14,6 +35,11 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    if (this.x > gameConfig.enemyEndX) {
+        this.x = gameConfig.enemyStartX;
+    }
+    this.x = this.x + (dt * this.speed);
+
 }
 
 // Draw the enemy on the screen, required method for game
@@ -21,15 +47,113 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
 
+// Generates a random Y coordinate for Enemy
+Enemy.prototype._getRandomY = function() {
+    var row = Math.floor( Math.random() * gameConfig.numEnemyRows);
+    return gameConfig.rowOffset + row * gameConfig.rowSpace;
+}
+
+
+// Generates a random Y coordinate for Enemy
+Enemy.prototype._getRandomSpeed = function() {
+    return 100 + Math.random() * gameConfig.enemySpeedRange;
+}
+
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.sprite = 'images/char-boy.png';
+    this.x = 2 * gameConfig.colSpace;
+    this.y = gameConfig.rowOffset + 4 * gameConfig.rowSpace;
+    this.step = {x: gameConfig.colSpace, y: gameConfig.rowSpace};
+}
+
+//Player object method to run for each frame
+Player.prototype.update = function() {
+    //collision detection
+    for(var i = 0; i < allEnemies.length; i++){
+        if(this._isCollision(allEnemies[i])){
+            this._resetLocation();
+        }
+    }
+}
+
+//Player object method to render player sprite on canvas
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+//Player object method to handle keyboard input
+Player.prototype.handleInput = function(keyCode) {
+    var newX = this.x;
+    var newY = this.y;
+    if(keyCode === 'left') {
+        newX = this.x - this.step.x;
+    };
+    if(keyCode === 'right') {
+        newX = this.x + this.step.x;
+    };
+    if(keyCode === 'up') {
+        newY = this.y - this.step.y;
+    };
+    if(keyCode === 'down') {
+        newY = this.y + this.step.y;
+    };
+
+    //reset player if in water
+    if(this._inWater(newX,newY)){
+        this._resetLocation();
+        return;
+    };
+
+    //check if new player position is in bounds
+    if(this._inBounds(newX,newY)){
+        this.x = newX;
+        this.y = newY;
+    };
+}
+
+//private method for Player object to detect collision with enemies
+Player.prototype._isCollision = function(enemy) {
+    if(Math.abs(this.x - enemy.x) < gameConfig.collisionThreshold.x &&
+        Math.abs(this.y - enemy.y) < gameConfig.collisionThreshold.y) {
+        return true;
+    }
+}
+
+//private method for Player object to reset location to bottom row
+Player.prototype._resetLocation = function() {
+    this.x = 2 * gameConfig.colSpace;
+    this.y = gameConfig.rowOffset + 4 * gameConfig.rowSpace;
+}
+
+//private method for Player object to check whether player location is valid
+Player.prototype._inBounds = function(x, y) {
+    if( x < 0 || x > 4 * gameConfig.colSpace || y < 0 || y > gameConfig.colOffset + 4 * gameConfig.rowSpace) {
+        return false;
+    }
+    return true;
+}
+
+//private method for Player object to check whether player is in river
+Player.prototype._inWater = function(x, y) {
+    if( x < 0 || x > 4 * gameConfig.colSpace || y < gameConfig.rowOffset) {
+        return true;
+    }
+    return false;
+}
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var player = new Player();
+var allEnemies = [];
+for(var i = 0; i < gameConfig.numEnemies; i++){
+    allEnemies.push(new Enemy());
+}
 
 
 // This listens for key presses and sends the keys to your
